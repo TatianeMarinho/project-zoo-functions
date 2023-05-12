0ff0dfe3b75356e9ce8d20d7c858d3feba3ee00b
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-12 15:57:14 -0400

    enviando o segundo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animals = data.species.find((specie) => animal.toLowerCase().includes(specie.name)).residents;
+
+  return animals.every((species) => species.age >= age);
 };
+
+getAnimalsOlderThan('tigers', 4);
 
 module.exports = getAnimalsOlderThan;
