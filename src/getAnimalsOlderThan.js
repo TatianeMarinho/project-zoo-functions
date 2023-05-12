2354cd52bf3e0e62c2d516fd4b34fa9c5278be71
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-12 16:50:05 -0400

    consertando o lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  const animals = data.species.find((specie) => animal.toLowerCase().includes(specie.name)).residents;
+  const animals = data.species.find((specie) =>
+    animal.toLowerCase().includes(specie.name)).residents;
 
   return animals.every((species) => species.age >= age);
 };
