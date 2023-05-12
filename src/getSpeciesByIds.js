a65ef5bb43c63f0983e2b8f5e18923edee53fc8a
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-12 14:28:41 -0400

    enviando a primeira

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
-  
-};
+const getSpeciesByIds = (...ids) => data.species.filter((specie) => ids.includes(specie.id));
+
+getSpeciesByIds();
 
 module.exports = getSpeciesByIds;
