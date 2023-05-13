f420d8a87849d5ecfa1aa0660a42d9f4f4ac1cd7
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-13 17:02:15 -0400

    enviando mesmo dando erro. nao sei o que aconteceu

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (...animal) => {
+  if (!animal) {
+    const animals = {};
+    data.species.forEach((specie) => { animals[specie.name] = specie.popularity; });
+
+    return animals;
+  }
+  return data.species.find((specie) =>
+    specie.name === animal.species).residents.reduce((acc, curr) => {
+    if (!animal.sex || curr.sex === animal.sex) return acc + 1;
+
+    return acc;
+  }, 0);
 };
+console.log(countAnimals({ species: 'penguins' }));
 
 module.exports = countAnimals;
