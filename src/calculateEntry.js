9bd6d828cdb19818a7010bb0ace4ab807e77b52e
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-13 21:23:47 -0400

    enviando mais ummmmmmm

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const initialValue = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      initialValue.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      initialValue.adult += 1;
+    } else {
+      initialValue.senior += 1;
+    }
+  });
+  return initialValue;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const total = (child * data.prices.child) + (adult * data.prices.adult)
+  + (senior * data.prices.senior);
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };
