2ee3998f222548c79496fdf598d8b531074c6889
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-15 14:22:05 -0400

    enviando a dez

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const specie = data.employees.find((person) => person.id === id).responsibleFor[0];
+
+  const animal = data.species.find((species) => species.id === specie);
+
+  const objectAnimal = animal.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+
+  const { name, sex, age } = objectAnimal;
+
+  return [name, sex, age];
 };
 
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
