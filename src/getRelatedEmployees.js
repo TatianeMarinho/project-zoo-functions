9ac5da41d2f6957a4c0addfad441c139a35abdf5
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-16 18:45:03 -0400

    enviando a quarta. finalmente

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-undef */
 const data = require('../data/zoo_data');
 
-const isManager = (id) => data.employees
-  .some((employe) => employe.managers.includes(id));
+const isManager = (id) => data.employees.some((employe) =>
+  employe.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId) === false) {
+  if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  } else {
-    data.employees.reduce((acc, curr) =>
-      (curr.managers === managerId ? acc.push(`${curr.firstName} ${curr.lastName}`) : acc), []);
   }
+  const result = [];
+  const employees = data.employees.filter((employe) => employe.managers.includes(managerId));
+  employees.forEach((employe) => result.push(`${employe.firstName} ${employe.lastName}`));
+  return result;
 };
 
 module.exports = { isManager, getRelatedEmployees };
