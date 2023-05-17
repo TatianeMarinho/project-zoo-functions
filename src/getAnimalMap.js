630d6a2cb8bb0a2120d3d33a38c73d010ea326cd
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-16 20:25:36 -0400

    minha cabeça ta saindo fumaçaaaaaa

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
+/*
+const animals = () =>
+  ({
+    NE: data.species.filter((specie) => specie.location === 'NE').map((specie) => specie.name),
+    NW: data.species.filter((specie) => specie.location === 'NW').map((specie) => specie.name),
+    SE: data.species.filter((specie) => specie.location === 'SE').map((specie) => specie.name),
+    SW: data.species.filter((specie) => specie.location === 'SW').map((specie) => specie.name),
+  }); */
+
+/* const animalsNames = () => {
+  const animals = animals();
+  animals.map((location) => {
+    //NE: [ 'lions', 'giraffes' ],
+    location.map((specieLocat) => {
+    data.species.every((specie) => specie.name === specieLocat)
+  })
+
+} */
 
 const getAnimalMap = (options) => {
-  // seu código aqui
+/*   if (!options || !options.includeNames) {
+    return animals();
+  } if (options.includeNames === true) {
+    return
+  } */
 };
 
+console.log(getAnimalMap({ sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
